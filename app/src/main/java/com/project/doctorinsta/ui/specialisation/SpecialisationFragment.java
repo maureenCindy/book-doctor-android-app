3bcab63d40abd3ec311cacd5b2ca81ba54895218
cindy <mpofusindie@gmail.com>
2020-11-11 01:11:33 +0200

    search doc

--- app/src/main/java/com/project/doctorinsta/ui/specialisation/SpecialisationFragment.java
+++ app/src/main/java/com/project/doctorinsta/ui/specialisation/SpecialisationFragment.java
@@ -12,12 +12,22 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.project.doctorinsta.R;
 import com.project.doctorinsta.adapter.SpecialityAdapter;
 import com.project.doctorinsta.data.Specialisation;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class SpecialisationFragment extends Fragment {
 
@@ -35,13 +45,37 @@ public class SpecialisationFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        List<Specialisation> specialisations = new ArrayList<>();
-        Log.d("spec from db"," :"+specialisations.toString());
-        linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        specialityAdapter = new SpecialityAdapter(getActivity(), specialisations);
-        recyclerView.addItemDecoration(new GridSpacingItemDecoration(1, 2, true));
-        recyclerView.setAdapter(specialityAdapter);
+         List<Specialisation> specialisations = new ArrayList<>();
+        DatabaseReference dbRef= FirebaseDatabase.getInstance().getReference("specialisations");
+        Query specList = dbRef.orderByChild("id");
+        specList.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NotNull DataSnapshot dataSnapshot) {
+                Log.d("found specialisations",":"+dataSnapshot.getChildrenCount());
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                    Log.d("found specialisation",":"+snapshot.toString());
+                    String nameFromDb =snapshot.child("name").getValue(String.class);
+                    String descFromDb =snapshot.child("description").getValue(String.class);
+                    Specialisation specialisation = new Specialisation();
+                    specialisation.setName(nameFromDb);
+                    specialisation.setDescription(descFromDb);
+                    specialisations.add(specialisation);
+                }
+                Log.d("found specialisations",":"+specialisations.size()+" for adapter");
+                linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+                recyclerView.setLayoutManager(linearLayoutManager);
+                specialityAdapter = new SpecialityAdapter(getActivity(), specialisations);
+                recyclerView.addItemDecoration(new GridSpacingItemDecoration(1, 2, true));
+                recyclerView.setAdapter(specialityAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "load Specialisation:onCancelled", databaseError.toException());
+                // ...
+            }
+        });
     }
 
     @Override
